a9b64dd742318eaee8d097c060352b1feb064738
Aki Tuomi <cmouse@cmouse.fi>
2015-05-07 16:34:26 +0300

    Rewrite queue mechanism

--- lib/mbot/queue.rb
+++ lib/mbot/queue.rb
@@ -1,67 +1,36 @@
 module MBot
-
-  MAX_BUCKET        = 5.0  # How many messages to burst
-  MAX_QUEUE         = 20   # How long until queue overflows
-  INTER_MESSAGE_GAP = 2.1  # How long to wait between sending messages
-
+  BURST = 4
+  DELAY = 2
+  MAX_QUEUE = 10
   class Queue
     attr_accessor :bot
-    attr_reader :tokens
     def initialize bot=nil
-      @bot        = bot
-      @queue      = Hash.new{|h,k|h[k]=[]}
-      @tokens     = MAX_BUCKET
-      @next_token = Time.now
+      @queue = []
+      @bot = bot
     end
-
     def add dst, msg
-      MBot.sleep.for :queue, 0.05
-      @queue[dst] << msg
+      MBot.sleep.for :queue, 0.1
+      @queue << [dst, msg]
     end
-
     def serve
-      taildrop
-      while @tokens>0 and msg=dequeue
-        @tokens -= 1
-        @bot.send_cmd 'PRIVMSG', msg[:target], msg[:msg]
-      end
-
-      if @tokens<MAX_BUCKET and Time.now>@next_token
-        @tokens += 1
-        @next_token = Time.now + INTER_MESSAGE_GAP
-        if @tokens < MAX_BUCKET
-          MBot.sleep.for :queue, INTER_MESSAGE_GAP
-        else
-          MBot.sleep.for :queue, nil
-        end
-      end
-    end
-   
-    def dequeue
-      if @queue.empty?
-        nil
-      else
-        target = @queue.keys.sample
-        msg    = @queue[target].shift
-        @queue.delete target if @queue[target].empty?
-        {:target=>target, :msg=>msg}
-      end
-    end
-
-    def taildrop
-      alarms = []
-      @queue.each do |target, msg|
-        if @queue[target].size > MAX_QUEUE
-          msgs  = @queue[target].shift @queue[target].size-MAX_QUEUE
-          alarms << "#{msgs.size} messages to #{target} dropped due to queue full"
-        end
-      end
-      alarms.each do |alarm|
-        @queue[CFG.bot[:control]].unshift alarm
+      MBot.sleep.for :queue, nil
+      if @queue.size > MAX_QUEUE
+        msgs = @queue.shift @queue.size-MAX_QUEUE
+        alarm = "#{msgs.size} messages dropped due to queue full"
+        @bot.send_cmd 'PRIVMSG', CFG.owner[:nick], alarm
         Log.warn alarm
+        msgs.each do |msg|
+          Log.warn msg.join ' -> '
+        end
+      end
+      @queue.shift(BURST).each do |msg|
+        @bot.send_cmd 'PRIVMSG', msg.first, msg.last
+      end
+      sleep DELAY+1 unless @queue.empty?
+      while msg = @queue.shift
+        @bot.send_cmd 'PRIVMSG', msg.first, msg.last
+        sleep DELAY
       end
     end
-
   end
-
 end
