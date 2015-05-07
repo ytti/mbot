deb17b1eb8bd9a0c560e5d6fab0497bf8f698a40
Aki Tuomi <cmouse@cmouse.fi>
2015-05-07 16:34:45 +0300

    Use integer for sleeping

--- lib/mbot/sleep.rb
+++ lib/mbot/sleep.rb
@@ -7,13 +7,12 @@ module MBot
       if not sec
         @req.delete requestor.to_sym
       else
-        @req[requestor.to_sym] = sec.to_f
+        @req[requestor.to_sym] = sec.to_i
       end
     end
-    def to_f
-      f = @req.sort_by { |k,v| v }.first
-      f ? f.last : CFG.sleep
+    def to_i
+      i = @req.sort_by { |k,v| v }.first
+      i ? i.last : CFG.sleep
     end
-    alias_method :to_i, :to_f
   end
 end
--- plugin/sleep.rb
+++ plugin/sleep.rb
@@ -1,5 +1,5 @@
 class Sleep < Plugin
   def to_bot
-    bot.say MBot.sleep.to_f.inspect
+    bot.say MBot.sleep.to_i.inspect
   end
 end
